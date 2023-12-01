b5ec1e0b278ec91cf984d720158f75e10c9a1a09
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2023-12-01 19:04:48 +0530

    Add files via upload

---
+++ premativedatatype.js
@@ -0,0 +1,24 @@
+// nn bb ss u 
+let a = null;
+let b = 456;
+let c = true;
+let d = BigInt("565") +BigInt("5")
+let e = "Gunjan"
+let f = Symbol("i am a nice symbol")
+let g = undefined
+
+console.log (a,b,c,d,e,f,g)
+console.log(typeof b)
+
+// objects
+
+const item = {
+"harry":77,
+"gunkan" : true,
+"jj": false,
+"rohan":undefined 
+}
+
+console.log(item["gunkan"])
+
+
