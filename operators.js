8414bc37f6f379a914a868fd30a9bd1803bdab87
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-10 00:12:27 +0530

    Add files via upload

---
+++ operators.js
@@ -0,0 +1,31 @@
+// operators in java script
+//console.log("operators in JS")
+// let a =50;
+// let b =4;
+
+/* let assignment = 1;
+assignment += 5
+console.log(assignment) */
+
+
+// compersion operators
+let comp1= 3;
+let comp2= 4;
+// console.log("comp1==comp2",comp1==comp2)
+
+// logical operators
+
+let x =5;
+let y = 6;
+console.log(x<y && x==5)
+console.log(x>y || x==5)
+console.log(!false)
+
+
+
+
+
+
+
+
+
