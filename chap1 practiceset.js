10e777a38a48af74a0248b5a8cca18cc6f245d24
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-11 00:40:13 +0530

    Add files via upload

---
+++ chap1 practiceset.js
@@ -0,0 +1,28 @@
+// que 1 
+let a= "Gunjan"
+let b = 67
+console.log(a+b)
+
+// que 2 
+console.log(typeof (a+b))
+
+//que 3 
+const a1 = {
+  name: "Gunjana"
+  
+  
+}
+// question 4 
+a1["friend"]= "Harry"
+a1["name"]="lovish"
+console.log(a1)
+
+// question 5 
+ const dict = {
+  appreciate : "to give validation",
+   approxi : "give a distance approch",
+  google : "your self",
+ }
+console.log(dict.approxi)
+console.log(dict["google"])
+ 
