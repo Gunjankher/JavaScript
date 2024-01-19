91be98bc3b94dfe9b3dd2161da4874c5a5417acd
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-19 17:16:49 +0530

    Create variables.js

---
+++ variables.js
@@ -0,0 +1,23 @@
+
+// variables 
+console.log("hello")
+console.log("how you doin")
+// var is also used but that is old fashioned 
+let a = 77 
+console.log (a)
+a= "harry" //it becomes a string not variable 
+console.log(a)
+
+
+// let 8harry =7 - this will throw an error numbers in variable are not allowed 
+
+// 2 .  const , let ,var 
+// var a= 54
+// var b= 56 
+// var a  = 54  // var can be redeclared  but let can not 
+// {
+// let b =' this '
+// } block ki batt block me hi rahengi  this is advantage of using let
+
+
+
