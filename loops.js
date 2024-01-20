3378ba442798b115ac9dc86c35ee2e806fe8c5b2
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-20 23:14:27 +0530

    Create loops.js

---
+++ loops.js
@@ -0,0 +1,29 @@
+/*for loop
+let sum=0 
+let n= prompt("enter your number")
+n= Number.parseInt(n)
+for ( i=0; i<n; i++) {
+  
+  sum += (i+1)
+}
+console.log("sum of these natural "+ n + "numbers is " + sum) */
+
+//for in loops 
+
+let obj = {
+  harry: 50,
+  shubh  : 67,
+  ritika  :78,
+shivalika  : 78,
+}
+for( let a in obj){
+  console.log(" marks of "+ a +" are "+ obj[a])
+} 
+// for of loop 
+for (let b of "Gunjan") {
+  console.log(b)
+}
+
+
+  
+
