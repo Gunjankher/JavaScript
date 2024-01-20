b8eb3d7f9ec9692ef5c46fc72c1676fbf5d7c179
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-20 23:15:54 +0530

    Create whileLoop.js

---
+++ whileLoop.js
@@ -0,0 +1,20 @@
+// factorial via while loop 
+n= prompt("enter your number")
+n= Number.parseInt(n)
+let i =5;
+let b =4; 
+while (i<=n) {
+b*=i
+ 
+  
+}
+
+console.log(b )
+
+//do while loop 
+do{
+  console.log(i)
+  i++;
+
+} while (i<n)
+// in do while loop blocks runs first and the condition is checked afterwards 
