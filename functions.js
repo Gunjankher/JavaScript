cbb7219718ef2e660fa378b5449ef394d7e5d3df
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-29 17:50:22 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,23 @@
+// functions in javascript
+function gk(x,y,z) {
+  console.log("ho gaya daddy")  
+return Math.round(69+(x*y*z)) // math.round uses for roundof of numbers 
+  
+}
+const sum= (p,q)=>{
+  return (p+q)
+}
+const hello= () =>{
+  console.log("hey how you doin")
+  return 'hi'
+}
+let a = 3
+let b=  4
+let c = 5 
+let v = hello()
+
+console.log ("multi of a ,b,c, is " ,gk(a ,b ,c))
+console.log ("multi of a ,b,c, is " ,gk(b ,c,a))
+console.log ("multi of a ,b,c, is " ,gk( c,a,b))
+console.log( sum  (56,7))
+console.log(v)
