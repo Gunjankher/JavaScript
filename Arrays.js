e8d5eea95be784121df34942fa536432a1e7c3be
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-29 17:53:25 +0530

    Create Arrays.js

---
+++ Arrays.js
@@ -0,0 +1,13 @@
+let marks_class_1=[69,67,78,98,null, "not present"]
+console.log(marks_class_1[0]) 
+console.log(marks_class_1[1])
+console.log(marks_class_1[2])
+console.log(marks_class_1[3])
+console.log(marks_class_1[4])
+console.log(marks_class_1[5])
+console.log(marks_class_1[6])
+console.log("the marks of this class is in number is ",marks_class_1.length) 
+marks_class_1[6]= 77// changing the value of an array 
+marks_class_1[0]=87 // changing the existing array vlaue
+console.log(marks_class_1[0])
+console.log(marks_class_1)
