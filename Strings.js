a5b3ea9ede8a2e23fb2080d3552db4d52564b20c
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-29 17:51:30 +0530

    Create Strings.js

---
+++ Strings.js
@@ -0,0 +1,15 @@
+// strings 
+let name = "How you doin"
+console.log(name.length)
+/*console.log(name[0])this gives h 
+console.log(name[1])  0   
+console.log(name[2])   w  */
+let friend = 'shubham'
+console.log(friend)
+
+// template literals 
+
+let boy1 = "Nikhil"
+let boy2 = "sagam"
+let sentance = `${boy2} is friend of ${boy1} `
+console.log(sentance)
