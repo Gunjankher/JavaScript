28ca5b360e1aa18af30535e2a9c6a9958c026325
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-29 17:52:49 +0530

    Create Stringsmethod.js

---
+++ Stringsmethod.js
@@ -0,0 +1,12 @@
+let name = "GUNJAN"
+/*console.log(name.length)
+console.log(name.toLowerCase())
+console.log(name.slice(1,5))
+console.log(name.slice(3))
+console.log(name.replace("GUN", "BUN")) */ 
+let friend = "murkh"
+console.log(name.concat (" is a enemy of ",friend) )
+
+
+
+
