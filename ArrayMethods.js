2b1a03adaf3cc98069f548e611861c91ab125389
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-29 17:54:32 +0530

    Create ArrayMethods.js

---
+++ ArrayMethods.js
@@ -0,0 +1,16 @@
+// Array methods 
+let num= [1,2,3,44]
+let b = num.toString()
+console.log(b)
+console.log(typeof b )
+let c = num.join("_")
+console.log(c,typeof c)
+//  let d= num.pop()
+// console.log(num,d)
+let d= num.push(56)
+console.log(num,d )
+// let r= num.shift()
+// console.log(r,num)
+let r= num.unshift(56)
+console.log(num,r)
+
