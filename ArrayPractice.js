c2da5602cf12069a79cfd82fa569dbf82c5a1594
Gunjan Kher <147532045+Gunjankher@users.noreply.github.com>
2024-01-29 17:58:17 +0530

    Create ArrayPractice.js

---
+++ ArrayPractice.js
@@ -0,0 +1,17 @@
+// practice problem 1 
+// make a array and take input form users 
+/*let arr=[ 2,65,78,2,3,]
+let a = prompt("enter your number")
+a= Number.parseInt(a) 
+arr.push(a)
+console.log(arr)*/
+
+// Practice problem 2 
+  let arr=[ 2,65,78,2,3,]
+let a;
+ do {
+   a = prompt("enter your number")
+a= Number.parseInt(a) 
+arr.push(a)
+} while(a!=0)
+console.log(arr)
