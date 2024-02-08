431b1719280b06ba287c1833f09167c972c4843b
Gunjankher <gkhercode@gmail.com>
2024-02-08 17:30:49 +0530

    alert

---
+++ alert prompt confirm/script.js
@@ -0,0 +1,14 @@
+alert("hello your script works yey")
+
+let a= prompt("enter your value of a here ","6969")
+a= Number.parseInt(a)
+ document.write(a)
+alert("you type the number of " + (typeof a))
+let write = confirm("do you wanna write it to the page")
+if(write){
+document.write()
+}
+else{
+  document.write(" please allow me to write")
+}
+
